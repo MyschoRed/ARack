d4ee74835db362c05b72827162dca9d5646bbd8c
Michal Spisiak <97632552+MyschoRed@users.noreply.github.com>
2023-08-02 15:39:36 +0200

    registracia

--- Core/urls.py
+++ Core/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='Core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('registration/', views.registration, name='registration'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -1,10 +1,25 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import QueryDict
 from django.shortcuts import render, redirect
 
-from Core.forms import SheetToPaletteForm
+from Core.forms import SheetToPaletteForm, SignUpForm
 from Core.models import Palette, PaletteSheet, Sheet
 
+
+def registration(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('welcome')
+    else:
+        form = SignUpForm()
+    return render(request, 'Core/registration.html', {'form': form})
 
 # na vytvorenie paliet
 def palette_generator():
